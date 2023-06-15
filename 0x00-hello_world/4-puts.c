710f09b914092f3caf0e238747bd348439f3b567
jnieh1990 <jnieh1990@gmail.com>
2023-06-15 14:29:54 -0700

    FIX 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
-int main(void){
-	puts ("\"Programming is like building a multilingual puzzle");
-	return 0;
+/* C program to write a string */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
