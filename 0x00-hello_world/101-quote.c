9179589d5e2d58399f542a2fc86108ead28dfd38
jnieh1990 <jnieh1990@gmail.com>
2023-06-15 17:14:46 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 /**
- * main : Entrée du programme
+ * main - Entrée du programme
  *
- * output : always return 1
+ * output - always return 1
  *
  */
 
 int main(void)
-	{
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 200);
-	return 1;
-	}
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 100);
+	return (1);
+}
