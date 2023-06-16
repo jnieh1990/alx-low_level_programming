c4cfe9e87a629b06c7555a5e83f0d72903407ddd
jnieh1990 <jnieh1990@gmail.com>
2023-06-16 08:54:13 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: alphabet
+ *
+ * Return: always true, always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	c = '0';
+	while (c <= '9')
+	{
+		putchar (c);
+		c = c + 1;
+	}
+	putchar ('\n');
+	return (0);
+}
