e3f167f1e3aefa65f32427da354ca91aa4458055
jnieh1990 <jnieh1990@gmail.com>
2023-06-16 02:20:32 -0700

    FIX 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,14 +4,14 @@
 /**
  * main - Entry point
  *
- * Description : Function that shows a message
+ * Description: Function that shows a message
  *
- * Return : Always 1 (success)
+ * Return: Always 1 (success)
  */
 int main(void)
 {
 	write(2,
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-		59);
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	59);
 	return (1);
 }
