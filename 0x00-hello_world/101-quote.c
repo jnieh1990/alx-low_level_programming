d0bd1c1c5aa4055ef1a7e87a295aee552cf05e1a
jnieh1990 <jnieh1990@gmail.com>
2023-06-15 17:01:21 -0700

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main : Entrée du programme
+ *
+ * output : always return 1
+ *
+ */
+
+int main(void)
+	{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 200);
+	return 1;
+	}
