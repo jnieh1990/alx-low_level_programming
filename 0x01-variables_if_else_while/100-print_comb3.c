f1c7c1bfa23b8bb66dbdb33e58fa47e44df19797
jnieh1990 <jnieh1990@gmail.com>
2023-06-16 17:26:28 -0700

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: alphabet
+ *
+ * Return: always true, always 0
+ */
+
+int main(void)
+{
+	int i,j;
+
+	i = 0;
+	while (i <= 9)
+	{
+		j = i + 1;
+		while (( i < j ) && (j <= 9))
+		{
+			putchar ('0' + i);
+			putchar ('0' + j);
+			j++;
+			if ((i == 8) && (j == 9))
+				break;
+			if ((i != 8) || (j != 9))
+			{
+			putchar (',');
+			putchar (' ');
+			}
+		}
+		i++;
+	}
+	putchar ('\n');
+	return (0);
+}
