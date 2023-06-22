aa96beeaeafafcf657c1c93c5e1ce4bb1cf54847
jnieh1990 <jnieh1990@gmail.com>
2023-06-22 12:55:42 -0700

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+/**
+ * _isupper - check if char is upper.
+ *
+ * @c: char to check
+ *
+ * Description: check lower
+ *
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+        if ((c >= 41) && (c < 91))
+                return (1);
+        else
+                return (0);
+}
