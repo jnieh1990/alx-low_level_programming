b34353bf5662e8fd5c2611338c2ee404e030445e
jnieh1990 <jnieh1990@gmail.com>
2023-06-22 13:17:39 -0700

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+/**
+ * _isdigit - check if char is upper.
+ *
+ * @c: char to check
+ *
+ * Description: check lower
+ *
+ * Return: 0 or 1
+ */
+
+int _isdigit(int c)
+{
+	if ((c >= 48) && (c < 59))
+		return (1);
+	else
+		return (0);
+}
