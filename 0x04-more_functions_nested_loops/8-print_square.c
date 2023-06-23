9248022501e44b01e8edd25be26a06a5fa8f2aed
jnieh1990 <jnieh1990@gmail.com>
2023-06-22 18:07:04 -0700

    8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_square - type the alphabet ten times
+ *
+ * @size: int
+ *
+ * Description: write alphabet
+ *
+ * Return: nothing
+ */
+
+void print_square(int size)
+	{
+
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size ; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+			}
+	}
+		else
+		_putchar('\n');
+	}
