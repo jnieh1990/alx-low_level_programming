d245e36f576a3a0aeed5dda9979d0d8e450b1b88
jnieh1990 <jnieh1990@gmail.com>
2023-06-23 03:14:26 -0700

    10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - type the alphabet ten times
+ *
+ * @size: int
+ *
+ * Description: write alphabet
+ *
+ * Return: nothing
+ */
+
+void print_triangle(int size)
+	{
+
+	int i, j, k;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size ; i++)
+		{
+			for (j = size - i; j > 1; j--)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k <= i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+			}
+		}
+	}
+
