4a4a44b536ede25710617cc0554d75f66fc7db1e
jnieh1990 <jnieh1990@gmail.com>
2023-06-22 17:34:11 -0700

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_diagonal - type the alphabet ten times
+ *
+ * @n: int
+ *
+ * Description: write alphabet
+ *
+ * Return: nothing
+ */
+
+void print_diagonal(int n)
+	{
+	
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n ; i++)
+		{
+			for (j = 1; j <= i ; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+			}
+		}
+		else
+		_putchar('\n');
+	}
+}
