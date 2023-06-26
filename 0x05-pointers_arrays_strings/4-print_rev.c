c58af783d62c7328f16e40963e3f1e984381644d
jnieh1990 <jnieh1990@gmail.com>
2023-06-26 14:22:26 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - reset
+ *
+ * Description: deferencement
+ *
+ * @s: char
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (i = i - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
