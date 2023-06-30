66d9168a1e77734a298d42e6adb37cc294b16fb1
jnieh1990 <jnieh1990@gmail.com>
2023-06-30 12:06:47 -0700

    5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - to upper
+ *
+ * @s: pointer to char
+ *
+ * Return: s
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] > 97 && s[i] < 122)
+			s[i] -= 32;
+		i++;
+	}
+	return (s);
+}
