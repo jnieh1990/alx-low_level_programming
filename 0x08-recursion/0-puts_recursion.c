72cb1a87fe32c383229c8ebc612a6795a8fe3f4e
jnieh1990 <jnieh1990@gmail.com>
2023-07-05 10:55:52 -0700

    123

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+/**
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		putchar('\n');
+}
