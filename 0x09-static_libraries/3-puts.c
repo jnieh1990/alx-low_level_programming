a38d43c9ffe291f7b161ab919dfad4d63449ce8d
jnieh1990 <jnieh1990@gmail.com>
2023-07-07 09:02:43 -0700

    0

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -4,6 +4,26 @@
 void _puts(char *str);
 
 /**
+ * _puts - reset
+ *
+ * Description: deferencement
+ *
+ * @str: char
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+        while (*str != '\0')
+        {
+                _putchar(*str);
+                str++;
+        }
+        _putchar('\n');
+}
+
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
