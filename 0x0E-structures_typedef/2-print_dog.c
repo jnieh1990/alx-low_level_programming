ce39ea1ac35255d311ddbac29eb3e4760f6bd478
jnieh1990 <jnieh1990@gmail.com>
2023-07-17 16:56:10 -0700

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 	if (d->name == NULL)
 		d->name = "(nil)";
 	if (d->age == NULL)
-                d->age = "(nil)";
+		d->age = 0;
 	if (d->owner == NULL)
 		d->owner = "(nil)";
 
